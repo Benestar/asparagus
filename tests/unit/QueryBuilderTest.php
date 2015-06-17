4206568a838c6851a04b906676cdc8b9f9c2ed5d
Bene <benestar.wikimedia@gmail.com>
2015-06-17 15:31:55 +0200

    Add more tests to QueryBuilderTest

--- tests/unit/QueryBuilderTest.php
+++ tests/unit/QueryBuilderTest.php
@@ -12,16 +12,141 @@ use Asparagus\QueryBuilder;
  */
 class QueryBuilderTest extends \PHPUnit_Framework_TestCase {
 
-	public function testConstruct() {
+	public function testPrefix() {
 		$queryBuilder = new QueryBuilder();
+		$this->assertSame(
+			$queryBuilder,
+			$queryBuilder->prefix( 'test', 'http://www.example.com/test#' )
+		);
 
-		$this->assertInstanceOf( 'Asparagus\QueryBuilder', $queryBuilder );
+		$this->assertContains(
+			'PREFIX test: <http://www.example.com/test#>',
+			$queryBuilder->getSPARQL()
+		);
 	}
 
-	public function testEmptyQuery() {
+	public function testSelect() {
+		$queryBuilder = new QueryBuilder();
+		$this->assertSame(
+			$queryBuilder,
+			$queryBuilder->select( 'a', 'b' )
+		);
+
+		$this->assertEquals(
+			'SELECT ?a ?b WHERE {}',
+			$queryBuilder->getSPARQL()
+		);
+	}
+
+	public function testSelect_invalidArgument() {
+		$queryBuilder = new QueryBuilder();
+		$this->setExpectedException( 'InvalidArgumentException' );
+
+		$queryBuilder->select( 'a', false );
+	}
+
+	public function testWhere() {
+		$queryBuilder = new QueryBuilder();
+		$this->assertSame(
+			$queryBuilder,
+			$queryBuilder->where( '?a', '?b', '?c' )
+		);
+	}
+
+	public function testPlus() {
+		$queryBuilder = new QueryBuilder();
+		$this->assertSame(
+			$queryBuilder,
+			$queryBuilder->plus( '?a', '?b', '?c' )
+		);
+	}
+
+	public function testPlus_knownSubject() {
+		$queryBuilder = new QueryBuilder();
+		$queryBuilder->where( '?a', '?b', '?c' );
+		$this->assertSame(
+			$queryBuilder,
+			$queryBuilder->plus( '?x', '?y' )
+		);
+	}
+
+	public function testPlus_knownPredicate() {
+		$queryBuilder = new QueryBuilder();
+		$queryBuilder->where( '?a', '?b', '?c' );
+		$this->assertSame(
+			$queryBuilder,
+			$queryBuilder->plus( '?z' )
+		);
+	}
+
+	public function testGroupBy() {
+		$queryBuilder = new QueryBuilder();
+		$this->assertSame(
+			$queryBuilder,
+			$queryBuilder->groupBy( 'test' )
+		);
+	}
+
+	public function testHaving() {
+		$queryBuilder = new QueryBuilder();
+		$this->assertSame(
+			$queryBuilder,
+			$queryBuilder->having( 'test' )
+		);
+	}
+
+	public function testOrderBy() {
+		$queryBuilder = new QueryBuilder();
+		$this->assertSame(
+			$queryBuilder,
+			$queryBuilder->orderBy( 'test' )
+		);
+	}
+
+	public function testLimit() {
+		$queryBuilder = new QueryBuilder();
+		$this->assertSame(
+			$queryBuilder,
+			$queryBuilder->limit( 5 )
+		);
+	}
+
+	public function testOffset() {
+		$queryBuilder = new QueryBuilder();
+		$this->assertSame(
+			$queryBuilder,
+			$queryBuilder->offset( 5 )
+		);
+	}
+
+	public function testGetSPARQL() {
 		$queryBuilder = new QueryBuilder();
 
 		$this->assertEquals( 'SELECT * WHERE {}', $queryBuilder->getSPARQL() );
+	}
+
+	public function testGetSPARQL_withPrefixes() {
+		$queryBuilder = new QueryBuilder( array( 'foo' => 'bar' ) );
+
+		$this->assertEquals( 'PREFIX foo: <bar> SELECT * WHERE {}', $queryBuilder->getSPARQL() );
+	}
+
+	public function testGetSPARQL_noPrefixes() {
+		$queryBuilder = new QueryBuilder( array( 'foo' => 'bar' ) );
+
+		$this->assertEquals( 'SELECT * WHERE {}', $queryBuilder->getSPARQL( false ) );
+	}
+
+	public function testToString() {
+		$queryBuilder = new QueryBuilder();
+
+		$this->assertEquals( 'SELECT * WHERE {}', strval( $queryBuilder ) );
+	}
+
+	public function testFormat() {
+		$queryBuilder = new QueryBuilder();
+
+		$this->assertEquals( "SELECT * WHERE {\n}\n", $queryBuilder->format() );
 	}
 
 }
