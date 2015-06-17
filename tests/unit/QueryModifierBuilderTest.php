2d4e18ccd10dc752dea79ab0892b1a45a8a9f139
Bene <benestar.wikimedia@gmail.com>
2015-06-17 14:31:02 +0200

    Test invalid arguments in modifiers

--- tests/unit/QueryModifierBuilderTest.php
+++ tests/unit/QueryModifierBuilderTest.php
@@ -19,11 +19,25 @@ class QueryModifierBuilderTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals( ' GROUP BY ?test', $queryBuilder->getSPARQL() );
 	}
 
+	public function testGroupByModifier_invalidArgument() {
+		$queryBuilder = new QueryModifierBuilder();
+		$this->setExpectedException( 'InvalidArgumentException' );
+
+		$queryBuilder->groupBy( null );
+	}
+
 	public function testHavingModifier() {
 		$queryBuilder = new QueryModifierBuilder();
 		$queryBuilder->having( 'test' );
 
 		$this->assertEquals( ' HAVING test', $queryBuilder->getSPARQL() );
+	}
+
+	public function testHavingModifier_invalidArgument() {
+		$queryBuilder = new QueryModifierBuilder();
+		$this->setExpectedException( 'InvalidArgumentException' );
+
+		$queryBuilder->having( null );
 	}
 
 	public function testOrderByModifier() {
@@ -40,6 +54,20 @@ class QueryModifierBuilderTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals( ' ORDER BY ?test DESC', $queryBuilder->getSPARQL() );
 	}
 
+	public function testOrderByModifier_invalidArgument() {
+		$queryBuilder = new QueryModifierBuilder();
+		$this->setExpectedException( 'InvalidArgumentException' );
+
+		$queryBuilder->orderBy( null );
+	}
+
+	public function testOrderByModifier_invalidDirection() {
+		$queryBuilder = new QueryModifierBuilder();
+		$this->setExpectedException( 'InvalidArgumentException' );
+
+		$queryBuilder->orderBy( 'test', 'FOO' );
+	}
+
 	public function testLimitModifier() {
 		$queryBuilder = new QueryModifierBuilder();
 		$queryBuilder->limit( 42 );
@@ -47,11 +75,25 @@ class QueryModifierBuilderTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals( ' LIMIT 42', $queryBuilder->getSPARQL() );
 	}
 
+	public function testLimitModifier_invalidArgument() {
+		$queryBuilder = new QueryModifierBuilder();
+		$this->setExpectedException( 'InvalidArgumentException' );
+
+		$queryBuilder->limit( null );
+	}
+
 	public function testOffsetModifier() {
 		$queryBuilder = new QueryModifierBuilder();
 		$queryBuilder->offset( 42 );
 
 		$this->assertEquals( ' OFFSET 42', $queryBuilder->getSPARQL() );
+	}
+
+	public function testOffsetModifier_invalidArgument() {
+		$queryBuilder = new QueryModifierBuilder();
+		$this->setExpectedException( 'InvalidArgumentException' );
+
+		$queryBuilder->offset( null );
 	}
 
 }
