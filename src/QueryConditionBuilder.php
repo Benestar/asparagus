be55a13bfadd1e1b1ea3bf96eb144c7ff39eaef3
Bene <benestar.wikimedia@gmail.com>
2015-06-19 14:26:59 +0200

    Basic subgraph support

--- src/QueryConditionBuilder.php
+++ src/QueryConditionBuilder.php
@@ -18,6 +18,26 @@ class QueryConditionBuilder {
 	private $conditions = array();
 
 	/**
+	 * @var string[] list of filters
+	 */
+	private $filters = array();
+
+	/**
+	 * @var QueryConditionBuilder[] list of optionals
+	 */
+	private $optionals = array();
+
+	/**
+	 * @var QueryConditionBuilder[] list of unions
+	 */
+	private $unions = array();
+
+	/**
+	 * @var QueryConditionBuilder[] other subgraphs
+	 */
+	private $subgraphs = array();
+
+	/**
 	 * @var string
 	 */
 	private $currentSubject = null;
@@ -26,6 +46,11 @@ class QueryConditionBuilder {
 	 * @var string
 	 */
 	private $currentPredicate = null;
+
+	/**
+	 * @var QueryConditionBuilder
+	 */
+	private $currentCondition = null;
 
 	/**
 	 * @var ExpressionValidator
@@ -39,11 +64,18 @@ class QueryConditionBuilder {
 	/**
 	 * Adds the given triple as a condition.
 	 *
-	 * @param string $subject
+	 * @param string|self $subject
 	 * @param string $predicate
 	 * @param string $object
+	 * @return self
+	 * @throws InvalidArgumentException
 	 */
 	public function where( $subject, $predicate, $object ) {
+		if ( $this->currentCondition !== null ) {
+			$this->subgraphs[] = $this->currentCondition;
+			$this->currentCondition = null;
+		}
+
 		$this->expressionValidator->validate( $subject,
 			ExpressionValidator::VALIDATE_PREFIXED_IRI | ExpressionValidator::VALIDATE_VARIABLE
 		);
@@ -57,6 +89,8 @@ class QueryConditionBuilder {
 		$this->currentSubject = $subject;
 		$this->currentPredicate = $predicate;
 		$this->conditions[$subject][$predicate][] = $object;
+
+		return $this;
 	}
 
 	/**
@@ -66,6 +100,8 @@ class QueryConditionBuilder {
 	 * @param string $subject
 	 * @param string|null $predicate
 	 * @param string|null $object
+	 * @return self
+	 * @throws InvalidArgumentException
 	 */
 	public function also( $subject, $predicate = null, $object = null ) {
 		if ( $predicate === null ) {
@@ -75,6 +111,41 @@ class QueryConditionBuilder {
 		} else {
 			$this->where( $subject, $predicate, $object );
 		}
+
+		return $this;
+	}
+
+	/**
+	 * Adds the given expression as filter.
+	 *
+	 * @param string $expression
+	 */
+	public function filter( $expression ) {
+		$this->expressionValidator->validate( $expression, ExpressionValidator::VALIDATE_FUNCTION );
+		$this->filters[] = $expression;
+	}
+
+	/**
+	 * Adds the given group graph pattern as optional.
+	 *
+	 * @param QueryConditionBuilder $conditionBuilder
+	 */
+	public function optional( QueryConditionBuilder $conditionBuilder ) {
+		$this->optionals[] = $conditionBuilder;
+	}
+
+	/**
+	 * Adds the given group graph pattern as union.
+	 *
+	 * @param QueryConditionBuilder $conditionBuilder
+	 */
+	public function union( QueryConditionBuilder $conditionBuilder ) {
+		if ( $this->currentCondition !== null ) {
+			$this->unions[] = $this->currentCondition;
+			$this->currentCondition = null;
+		}
+
+		$this->unions[] = $conditionBuilder;
 	}
 
 	/**
@@ -91,6 +162,11 @@ class QueryConditionBuilder {
 			$sparql .= $this->formatPredicates( $predicates ) . ' .';
 		}
 
+		$sparql .= $this->formatFilters();
+		$sparql .= $this->formatOptionals();
+		$sparql .= $this->formatUnions();
+		$sparql .= $this->formatSubgraphs();
+
 		return $sparql;
 	}
 
@@ -100,10 +176,35 @@ class QueryConditionBuilder {
 		}, array_keys( $predicates ), $predicates ) );
 	}
 
+	private function formatFilters() {
+		return implode( array_map( function( $filter ) {
+			return ' FILTER ' . $filter;
+		}, $this->filters ) );
+	}
+
+	private function formatOptionals() {
+		return implode( array_map( function( QueryConditionBuilder $conditionBuilder ) {
+			return ' OPTIONAL {' . $conditionBuilder->getSPARQL() . ' }';
+		}, $this->optionals ) );
+	}
+
+	private function formatUnions() {
+		return implode( ' UNION', function( QueryConditionBuilder $conditionBuilder ) {
+			return ' {' . $conditionBuilder->getSPARQL() . ' }';
+		}, $this->unions );
+	}
+
+	private function formatSubgraphs() {
+		return implode( array_map( function( QueryConditionBuilder $conditionBuilder ) {
+			return ' {' . $conditionBuilder->getSPARQL() . ' }';
+		}, $this->subgraphs ) );
+	}
+
 	/**
 	 * @return string[]
 	 */
 	public function getPrefixes() {
+		// @todo include subgraphs
 		return $this->expressionValidator->getPrefixes();
 	}
 
@@ -111,6 +212,7 @@ class QueryConditionBuilder {
 	 * @return string[]
 	 */
 	public function getVariables() {
+		// @todo include subgraphs
 		return $this->expressionValidator->getVariables();
 	}
 
